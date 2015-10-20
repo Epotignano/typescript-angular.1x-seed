baef95c115fe0a5fe9b41179f720eccf12623724
epotignano <epotignano@gmail.com>
2015-10-20 00:50:29 -0300

    init

---
+++ src/app/index.run.ts
@@ -0,0 +1,11 @@
+module smileMotivationz {
+  'use strict';
+
+  export class RunBlock {
+    /** @ngInject */
+    constructor($log: ng.ILogService) {
+      $log.debug('runBlock end');
+    }
+
+  }
+}
