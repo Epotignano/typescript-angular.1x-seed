dd91ad3c66f6dedc581fd9814d92ac4ceb5bf0bd
epotignano <epotignano@gmail.com>
2015-11-06 11:27:32 -0300

    Radio button formly.

--- src/app/index.formly.conf.ts
+++ src/app/index.formly.conf.ts
@@ -10,13 +10,24 @@ module smileMotivationz {
 
     static inject = ['formlyConfig'];
 
-    /* Formly configuration sector */
+    /* Formly general configuration sector */
 
     constructor(formlyConfig : any) {
       formlyConfig.setType({
         name: 'input',
-        template:'<md-input-container><label>{{ templateOptions.label }}</label> <input ng-model="model[options.key]"></md-input-container>'
-      })
+        template:'<md-input-container><label>{{ options.templateOptions.label }}</label> <input ng-model="model[options.key]"></md-input-container>'
+      });
+
+      formlyConfig.setType({
+        name:'textarea',
+        template: '<md-input-container flex><label>{{ options.templateOptions.label }}</label> <textarea ng-model="model[options.key]"></textarea></md-input-container>'
+      });
+
+      formlyConfig.setType({
+        name: 'radio',
+        template: '<md-radio-group ng-model="model[options.key]"><md-radio-button  class="md-primary" ng-repeat="(key, option) in options.templateOptions.options"id="{{$index}}"value="{{ option.value}}">{{option.name }}</md-radio-button></md-radio-group>'
+      });
+
 
     }
 
